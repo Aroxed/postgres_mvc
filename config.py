6845075506c47ed3349f902e945a7fe1a85aa728
Andrii Petrashenko <petrashenko@gmail.com>
2023-10-28 12:46:04 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,9 @@
+import os
+DB_LABEL = 'db42'
 DATABASE = {
-    "HOST": "localhost",
-    "DATABASE": "postgres",
-    "USER": "postgres",
-    "PASSWORD": "1"
+        "NAME": os.environ.get("DB_%s_NAME" % DB_LABEL),
+        "USER": os.environ.get("DB_%s_USERNAME" % DB_LABEL),
+        "PASSWORD": os.environ.get("DB_%s_PASSWORD" % DB_LABEL),
+        "HOST": os.environ.get("DB_%s_HOST" % DB_LABEL),
+        "PORT": os.environ.get("DB_%s_PORT" % DB_LABEL)
 }
